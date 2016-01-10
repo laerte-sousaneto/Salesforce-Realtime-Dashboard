21fd5ba407a73f497104e09a5636b2d80efe4283
Laerte Sousa Neto <laerte.sousaneto@gmail.com>
2016-01-10 02:06:37 -0500

    Case sensitive name change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes/mainRoutes')(app, express);
-var SalesforceApi = require('./server/api/SalesforceOrgApi')(app);
+var SalesforceApi = require('./server/api/SalesforceOrgAPI')(app);
 
 module.exports = app;
