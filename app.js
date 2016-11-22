f9bdd4b8005f9c2d383ac5c7294e10412afd1f7b
Laerte Sousa Neto <laerte.sousaneto@gmail.com>
2016-11-22 10:51:44 -0500

    cherry pick conflicts

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ var express = require('express');
 var app = express();
 var routes = require('./routes/mainRoutes')(app, express);
 var SalesforceApi = require('./server/api/SalesforceOrgAPI')(app);
+// var mongotest = require('./server/database/mongotest');
+
+//mongotest.insertUser();
+// mongotest.getUsers();
 
 module.exports = app;
